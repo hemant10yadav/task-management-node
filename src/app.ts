b8a81ef567b2e4cbbaf020249193f050e62d5aa0
Hemant Singh Yadav <98798073+hemant10yadav@users.noreply.github.com>
2024-03-19 19:26:59 +0530

    Initial setup of node js application with typescript support

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response, Application } from "express";
+import dotenv from "dotenv";
+import taskRoute from "./routes/taskRoute";
+import bodyParser from "body-parser";
+
+//For env File
+dotenv.config();
+
+const app: Application = express();
+const port = process.env.PORT || 8000;
+
+app.use(bodyParser.json());
+
+app.use(taskRoute);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to Express & TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`Server is Fire at http://localhost:${port}`);
+});
